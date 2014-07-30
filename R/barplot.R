723d31ace8b5d1262627532d8359ba8fbdd97ab0
Guangchuang Yu <guangchuangyu@gmail.com>
2014-07-30 18:15:15 +0800

    bug fixed of barplot

--- R/barplot.R
+++ R/barplot.R
@@ -23,17 +23,18 @@
 ##' @examples
 ##' df <- data.frame(Normal=c(0.83,0.79,0.99,0.69), Cancer=c(0.56,0.56,0.64,0.52))
 ##' barplot(df, barIndex=c(1,2), label="*")
-##' @author ygc
+##' @author Guangchuang Yu
 barplot <- function(data, title="", xlab="", ylab="", barIndex=NULL, label="*") {
     m <- colMeans(data)
     s <- apply(data, 2, sd)
     d <- data.frame(sample=names(m), mean=m, sd=s)
     d$sample <- factor(d$sample, levels=colnames(data))
 
-    p <- ggplot(d, aes(sample, mean, fill=sample, width=0.5))
+    p <- ggplot(d, aes(sample, mean))
     p <- p+geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd, width=0.2),
                          position=position_dodge(width=0.8))
-    p <- p+geom_bar(stat="identity", position=position_dodge(width=0.8)) ##, colour="black")
+    p <- p+geom_bar(aes(fill=sample), stat="identity",
+                    position=position_dodge(width=0.8)) ##, colour="black")
     ## p <- p+scale_fill_manual(values=c("grey80", "white"))
     ## p <- p+scale_y_continuous(expand=c(0,0), limits=c(0,1.2), breaks=seq(0,1.2, by=0.2))
     
@@ -44,7 +45,7 @@ barplot <- function(data, title="", xlab="", ylab="", barIndex=NULL, label="*") 
         if (length(barIndex) != 2) {
             warning("barIndex should be of length 2...")
         } else {
-            p <- addLabel(p, barIndex, label)
+            p <- addLabel(p, barIndex=barIndex, label=label)
         }
     }
     
@@ -54,14 +55,17 @@ barplot <- function(data, title="", xlab="", ylab="", barIndex=NULL, label="*") 
 ##' @importFrom ggplot2 geom_segment
 ##' @importFrom ggplot2 annotate
 ##' @importFrom ggplot2 aes
-addLabel <- function(p, barIndex=c(1,2), label="*") {
+addLabel <- function(p, barIndex, label="*") {
     d <- p$data
     lw <- d$mean+d$sd
-    lw <- lw[barIndex] * 1.02
+    lw <- lw[barIndex] * 1.05
     up <- max(lw * 1.1)
-    p <- p+geom_segment(aes(x=barIndex, y=lw, xend=barIndex, yend=max(up)))
-    p <- p+geom_segment(aes(x=barIndex[1], y=up, xend=barIndex[2], yend=up))
-    pos <- up - (up - max(lw))/6
+ 
+    p <- p+geom_segment(x=barIndex[1], y=lw[1], xend=barIndex[1], yend=up)
+    p <- p+geom_segment(x=barIndex[2], y=lw[2], xend=barIndex[2], yend=up)
+    p <- p+geom_segment(x=barIndex[1], y=up, xend=barIndex[2], yend=up)
+    pos <- up - (up-max(lw))/6
     p <- p+annotate("text", x=mean(barIndex),y=pos, label=label)
+    p <- p+scale_y_continuous(expand=c(0,0), limits=c(0, up*1.05))
     return(p)
 }
